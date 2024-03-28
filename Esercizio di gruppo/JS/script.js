a5eaa9f7f940c6b3b8426e949eb71769e6ccd7b2
Michael <152283283+JustDarkWish@users.noreply.github.com>
2024-03-28 16:41:28 +0100

    Editato index e script.

---
+++ Esercizio di gruppo/JS/script.js
@@ -0,0 +1,12 @@
+document.querySelector('#btnInserisci').addEventListener('click', function() {
+    let task = document.querySelector('#formContainer input[type="text"]').value;
+    let data = document.querySelector('#formContainer input[type="date"]').value;
+
+    if(task === '' || data === '') {
+        alert('Per favore, inserisci sia la task che la data.');
+        return;
+    }
+
+    let li = document.createElement('li');
+    li.textContent = task + ' - ' + data;
+})
