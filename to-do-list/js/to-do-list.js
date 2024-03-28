320da420f62689d6d4ff7f6f6d7cebcdeb4b4cce
Claudia <161829778+ClaudiaPezzoli@users.noreply.github.com>
2024-03-28 18:34:27 +0100

    Claudia
    add script

---
+++ to-do-list/js/to-do-list.js
@@ -0,0 +1,81 @@
+//prendo elementi html:
+let formContainer = document.querySelector("#formContainer");
+let todoContainer = document.querySelector("#todoContainer"); 
+
+let feedDate = document.querySelector("#feedDate");
+let feedItem = document.querySelector("#feedItem");
+let demo = document.querySelector("#demo");
+
+let task = document.querySelector("#task");
+let date = document.querySelector("#date");
+let feed = document.querySelector("#feed");
+
+let btnInserisci = document.querySelector("#btnInserisci");
+let btnModifica = document.querySelector("#btnModifica");
+let btnCancella = document.querySelector("#btnCancella");
+
+// let lisTask = document.querySelectorAll("ul#listaTask"); //restituisce una Nodelist
+
+
+let listaTask = []; 
+
+class Task{
+    constructor(nome, date){
+        this.nome = nome;
+        this.date = date;
+    }
+
+    stampaTask(){
+        let descrizioneTask = `<span>${this.nome} </span> <span> ${this.date} </span>`;
+        return descrizioneTask; 
+    } 
+}
+
+function recuperaTask(){
+    let erroreRecuperaTask = ""; //preparo variabile errore
+    if(task.value.trim() != "" && date.value.trim() != ""){
+        miaTask = new Task (task.value, date.value);
+        listaTask.push(miaTask); //inserisco nell'array di task
+    }else {
+        erroreRecuperaTask = `<p> Non hai compilato correttamente i form </p>`
+    }
+    //resetto campi di input:
+    task.value = "";
+    date.value = ""; 
+    return erroreRecuperaTask;
+}
+
+
+function caricaTask(){
+    let erroreUtenteMessaggio = recuperaTask();
+    if(erroreUtenteMessaggio.length != 0){ 
+        feed.innerHTML = `<p> Errore </p>`;
+    }else{
+        stampaListaTask();
+    }
+    return erroreUtenteMessaggio;  
+}
+
+function stampaListaTask(){
+    demo.innerHTML = "";
+    if(listaTask.length == 0){ //lunghezza stringa 0
+        feedItem.innerHTML = `<p> Non hai scritto la task </p>`; 
+    }else{
+        listaTask.forEach(task =>{
+            demo.innerHTML += `<li> ${task.stampaTask()} </li>`
+        })
+    }
+}
+
+btnInserisci.addEventListener('click', caricaTask);
+
+//dalla nodeList di listaTask
+[...listaTask].forEach(task => {
+    task.addEventListener("click", function(){ //metodo che quando clicco leva e mette sottolineature 
+        if(task.classList.contains('underline')){
+            task.classList.remove('underline');
+        }else {
+            task.classList.add('underline'); 
+        }
+    })
+})
