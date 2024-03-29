4f707cbf1533fc0c5a23d81144149cd8a75e58ec
Michael <152283283+JustDarkWish@users.noreply.github.com>
2024-03-29 15:23:38 +0100

    Eliminato un vecchio script commentato nel file js

--- Esercizio di gruppo/JS/script.js
+++ Esercizio di gruppo/JS/script.js
@@ -1,22 +1,3 @@
-/* document.querySelector('#btnInserisci').addEventListener('click', function() {
-    let task = document.querySelector('#formContainer input[type="text"]').value;
-    let data = document.querySelector('#formContainer input[type="date"]').value;
-
-    if(task === '' || data === '') {
-        alert('Per favore, inserisci sia la task che la data.');
-        return;
-    }
-
-    let li = document.createElement('li');
-    li.textContent = task + ' - ' + data;
-
-    document.querySelector('#listaTask').appendChild(li);
-
-    document.querySelector('#task').value = '';
-    document.querySelector('#data').value = '';
-
-}) */
-
 let containerMain = document.querySelector('#containerMain');
 let formContainer = document.querySelector('#formContainer');
 let task = document.querySelector('#task');
